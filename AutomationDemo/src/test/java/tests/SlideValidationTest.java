6805c8734552c5bd0f752c17e1130383b080aeec
HariUserGit <thatavarthihariprasad@gmail.com>
2025-09-07 16:53:31 +0530

    Allure reports added

--- AutomationDemo/src/test/java/tests/SlideValidationTest.java
+++ AutomationDemo/src/test/java/tests/SlideValidationTest.java
@@ -1,44 +1,38 @@
 package tests;
 
-import java.util.concurrent.TimeUnit;
-
-import javax.xml.datatype.Duration;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import base.BaseTest;
+import io.qameta.allure.*;
+
 import pages.HomePage;
 import utils.ExpectedTitles;
-import base.BaseTest;
 
 public class SlideValidationTest extends BaseTest {
 
-	 @Test
-	 public void validateSlideTitles() 
-	 {
-		 driver.get("https://www.nba.com/sixers/");
-		 HomePage homePage = new HomePage(driver);
-		 String[] actualTitles = homePage.getSlideTitles();
-		 String[] expectedTitles = ExpectedTitles.titles;
-		
-		 System.out.println("Total Slides Found: " + actualTitles.length);
-		
-		 for (int i = 0; i < actualTitles.length; i++) {
-		 System.out.println("Slide " + (i + 1) + ": " + actualTitles[i]);
-		 if (i < expectedTitles.length) 
-		 {
-			 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);;
-			 Assert.assertEquals(actualTitles[i], expectedTitles[i], "Mismatch at Slide " + (i + 1));
-			 } else 
-			 {
-				 System.out.println("No expected title provided for Slide " + (i + 1));
-			 }
-		 }
-	 }
+    @Test(description = "Validate Slide Titles on NBA Sixers Page")
+    @Epic("NBA Automation")
+    @Feature("Home Page Slides")
+    @Story("Validate Slide Titles")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("This test verifies that slide titles match the expected ones from ExpectedTitles class.")
+    public void validateSlideTitles() throws InterruptedException {
+        driver.get("https://www.nba.com/sixers/");
+        HomePage homePage = new HomePage(driver);
+        String[] actualTitles = homePage.getSlideTitles();
+        String[] expectedTitles = ExpectedTitles.titles;
 
+        System.out.println("Total Slides Found: " + actualTitles.length);
+
+        for (int i = 0; i < actualTitles.length; i++) {
+            System.out.println("Slide " + (i + 1) + ": " + actualTitles[i]);
+            if (i < expectedTitles.length) {
+                Thread.sleep(2000);
+                Assert.assertEquals(actualTitles[i], expectedTitles[i], "Mismatch at Slide " + (i + 1));
+            } else {
+                System.out.println("No expected title provided for Slide " + (i + 1));
+            }
+        }
+    }
 }
